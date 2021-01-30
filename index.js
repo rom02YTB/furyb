807e801717e1404f7dbe7b2db069d6eeb376ff04
rom02YTB <54501885+rom02YTB@users.noreply.github.com>
2021-01-30 15:15:18 +0100

    b

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require("discord.js");
+
+const Client = new Discord.Client;
+
+Client.on("ready", () => {
+    console.log("bot on");
+})
+
+
+Client.login("NzA5MTE3ODQ4MzkxMjU0MDk2.XrhPdQ.n_caV_z-g6PmRgkl9kU4lRyNpYI");
